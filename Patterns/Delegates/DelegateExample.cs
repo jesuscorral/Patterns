5f3978b3f8405490bb34e8ed6d834686e36132af
corralperez <corralperez@gmail.com>
2020-11-24 20:12:11 +0100

    Added delegates

---
+++ Patterns/Delegates/DelegateExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class DelegateExample
+    {
+        public delegate void ImprimirDelegado(string s);
+
+        Action<int, int, int> Check = DoWorkWithThreeParameters;
+        Action<int> PrintAge = (age) => Console.WriteLine($"La edad es: {age}");
+
+        Func<int, int, int> Multiplication = (v1, v2) => v1 * v2;
+
+        Predicate<int> IsLegal = (age) => age >= 18;
+
+        public DelegateExample()
+        { }
+
+        public void Delegate()
+        {
+            var d = new ImprimirDelegado(Imprime);
+            d("Esto es lo que hay que imprimir");
+        }
+
+        public void Action()
+        {
+            Check(10, 15, 23);
+            PrintAge(52);
+        }
+
+        public static void DoWorkWithThreeParameters(int arg1, int arg2, int arg3)
+        {
+            Console.WriteLine(arg1 + "-" + arg2 + "-" + arg3);
+        }
+
+        public void Function()
+        {
+            int valor = Multiplication(3, 2);
+            Console.WriteLine($"El resultado es {valor}");
+        }
+
+        public void Predicate()
+        {
+            Console.WriteLine($"Es mayor de edad?: {IsLegal(18)}");
+        }
+
+        private void Imprime(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Patterns/Delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var del = new DelegateExample();
+            del.Delegate();
+            del.Action();
+            del.Function();
+            del.Predicate();
+        }
+    }
+}
